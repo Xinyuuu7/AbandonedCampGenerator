240479e612c7ba35fb390eea3728e3495f4aebff
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2026-08-31 03:16:46 +0800

    Add files via upload

---
+++ AbandonedCampGenerator/src/main/java/AbandonedCampGenerator/jigsawpools/AbandonedCampJigsawBlocks.java
@@ -0,0 +1,10 @@
+package AbandonedCampGenerator.jigsawpools;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AbandonedCampJigsawBlocks {
+    public static final HashMap<String, List<JigsawBlock>> JIGSAW_BLOCKS = new HashMap<>() {{
+
+    }};
+}
---
+++ AbandonedCampGenerator/src/main/java/AbandonedCampGenerator/jigsawpools/JigsawBlock.java
@@ -0,0 +1,20 @@
+package AbandonedCampGenerator.jigsawpools;
+
+import com.seedfinding.mccore.util.block.BlockDirection;
+import com.seedfinding.mccore.util.pos.BPos;
+
+public class JigsawBlock {
+    public PoolType poolType;
+    public String name;
+    public String targetName;
+    public BlockDirection direction;
+    public BPos relativePos;
+
+    public JigsawBlock(PoolType poolType, String name, String targetName, BlockDirection direction1, BPos relativePos) {
+        this.poolType = poolType;
+        this.name = name;
+        this.targetName = targetName;
+        this.direction = direction1;
+        this.relativePos = relativePos;
+    }
+}
